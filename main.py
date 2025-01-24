aaefa61542a4daacec8b95c3f19bc9c66f027763
buithehai1994 <72820310+buithehai1994@users.noreply.github.com>
2025-01-24 11:57:00 +1100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
-from datetime import datetime
+from datetime import datetime, timedelta
 from tqdm import tqdm
 import json
 
@@ -15,7 +15,7 @@ dict_news = {"business": 'https://feeds.bbci.co.uk/news/business/rss.xml?edition
               "health": 'https://feeds.bbci.co.uk/news/health/rss.xml?edition=uk',
               "technology": 'https://feeds.bbci.co.uk/news/technology/rss.xml?edition=uk',
               "world":'https://feeds.bbci.co.uk/news/world/rss.xml?edition=uk',
-            "science_and_environment": 'https://feeds.bbci.co.uk/news/science_and_environment/rss.xml?edition=uk'}
+              "science_and_environment": 'https://feeds.bbci.co.uk/news/science_and_environment/rss.xml?edition=uk'}
 
 # Create the main class instance
 filtered_articles = FilteredArticles(dict_news)
@@ -28,11 +28,12 @@ def main():
         # Fetch webpage metadata
         df_metadata = filtered_articles.fetch_webpage_metadata()
 
-        # Filter articles published today
-        df_filtered = filtered_articles.filter_by_date()
+        # Filter articles published yesterday
+        yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+        df_filtered = filtered_articles.filter_by_date(date_filter=yesterday)
 
         # Save the filtered DataFrame as a JSON file to be pushed to GitHub
-        file_path = f'processed_files/bbc_articles_{datetime.today().strftime("%Y-%m-%d")}.json'
+        file_path = f'processed_files/bbc_articles_{yesterday}.json'
         result_dict = filtered_articles.convert_to_json(df_filtered, file_path)
         
         return result_dict  # Return the dictionary
